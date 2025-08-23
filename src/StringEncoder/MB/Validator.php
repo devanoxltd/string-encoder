5ad445e00fd7234fde62d4da97457470d56771da
Mr Chetan <contact@mrchetan.com>
2025-08-23 11:35:59 +0530

    Refactor encoding validation logic in Validator class and remove unused ExampleTest

--- src/StringEncoder/MB/Validator.php
+++ src/StringEncoder/MB/Validator.php
@@ -22,21 +22,16 @@ class Validator
     public function determineEncoding(string $encoding, bool $caseSensitive): ?string
     {
         $encodingList = mb_list_encodings();
+        $inputEncoding = $caseSensitive ? $encoding : mb_convert_case($encoding, MB_CASE_LOWER);
 
         foreach ($encodingList as $validEncoding) {
-            if ($validEncoding === $encoding || (
-                $caseSensitive === false &&
-                mb_convert_case($validEncoding, MB_CASE_LOWER) === mb_convert_case($encoding, MB_CASE_LOWER)
-            )
-            ) {
-                return $validEncoding;
-            }
-            if ($this->validateEncodingAlias($encoding, $validEncoding, $caseSensitive)) {
+            $compareEncoding = $caseSensitive ? $validEncoding : mb_convert_case($validEncoding, MB_CASE_LOWER);
+
+            if ($compareEncoding === $inputEncoding) {
                 return $validEncoding;
             }
         }
 
-        // no valid encoding string found that matches
         return null;
     }
 
@@ -52,36 +47,5 @@ class Validator
         }
 
         return $encoding === $expectedEncoding;
-    }
-
-    /**
-     * @internal
-     */
-    private function validateEncodingAlias(string $encoding, string $validEncoding, bool $caseSensitive): bool
-    {
-        if ($caseSensitive) {
-            $aliasEncoding = mb_encoding_aliases($validEncoding);
-        } else {
-            $encoding = mb_convert_case($encoding, MB_CASE_LOWER);
-            $aliasEncoding = $this->lowerCaseArray(mb_encoding_aliases($validEncoding));
-        }
-
-        return in_array($encoding, $aliasEncoding);
-    }
-
-    /**
-     * @internal
-     *
-     * @param  string[]  $encodings
-     * @return string[]
-     */
-    private function lowerCaseArray(array $encodings): array
-    {
-        $newEncodings = [];
-        foreach ($encodings as $key => $encoding) {
-            $newEncodings[$key] = mb_convert_case($encoding, MB_CASE_LOWER);
-        }
-
-        return $newEncodings;
     }
 }
